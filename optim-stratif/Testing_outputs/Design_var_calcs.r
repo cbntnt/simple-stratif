bda56d40dfdb4df4662ce41aa71f38909bb25ba4
Ichsani.Wheeler <Ichsani.Wheeler@gmail.com>
2012-08-07 10:15:46 +0000

    fluff plus minor stuff :)

--- optim-stratif/Testing_outputs/Design_var_calcs.r
+++ optim-stratif/Testing_outputs/Design_var_calcs.r
@@ -11,7 +11,7 @@ alloc = list(q1 = 0.5, q2 = 0, q3 = 0.5), takenone = 0,
 bias.penalty = 1, takeall = 0, rh = rep(1, Ls),
 model = c("none"),
 model.control = list(), algo = c("Kozak"),
-algo.control = list(method="modified")
+algo.control = list(method="modified", minNh=3)
 )
 
 outpt <- data.frame(strata = seq(along=x$Nh), Nh = x$Nh, Ah = x$Nh/sum(x$Nh), nh = x$nh, initbh = c((min(PRED_OC$oc_kgm3)), x$initbh), bh = c((min(PRED_OC$oc_kgm3)), x$bh), p_var = x$varh, smpvar = x$varh/x$nh, desvar = (x$Nh/(sum(x$Nh)))^2 * (x$varh/x$nh))
